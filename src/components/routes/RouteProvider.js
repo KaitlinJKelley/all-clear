4c3253064a56b9d425da097ea1c689e335f71ac7
kjk1325e <kaitlinjkelley@gmail.comm>
2021-03-23 18:26:37 -0500

    working to add paths table

--- src/components/routes/RouteProvider.js
+++ src/components/routes/RouteProvider.js
@@ -1,10 +1,12 @@
-import React, { createContext, useState } from "react"
+import React, { createContext, useEffect, useState } from "react"
 import { getRouteStreetNames } from "../../modules/RouteStreetNames"
 
 export const RouteContext = createContext()
 
 export const RouteProvider = (props) => {
     const [routes, setRoutes] = useState([])
+
+    const [routeForRoutePath, setRouteForRoutePath] = useState({})
 
     //  Use HERE GeoCoding and Search API to convert street addresses to lat/long pair
     const getLatLong = (address) => {
@@ -27,13 +29,13 @@ export const RouteProvider = (props) => {
             },
             body: JSON.stringify(routeObj)
         })
-        .then(getRoutes)
+            .then(getRoutes)
     }
     //set routes state variable equal to an array of all routes 
     const getRoutes = () => {
         return fetch("http://localhost:8088/routes")
             .then(res => res.json())
-            .then(routes => setRoutes(routes))
+            .then(routesArray => setRoutes(routesArray))
     }
 
     const getRouteById = (routeObjId) => {
@@ -76,6 +78,62 @@ export const RouteProvider = (props) => {
             .then(() => getDirections(originLatLong, destinationLatLong))
             // Returns an array of strings, where wach string is the next street a user should take 
             .then(directions => getRouteStreetNames(directions))
+
+    }
+
+    useEffect(() => {
+        // Try to put full function inside useEffect dependent on routeForRoutePath. 
+        // state variable will be set on add and on update to trigger useEffect
+        // Add conditional to make sure variable is not an empty object
+        // set variable back to initial state after new path is posted?
+    }, []) 
+
+    const createNewRoutePathObj = (routeObj) => {
+        let origin = routeObj.origin
+        let destination = routeObj.destination
+        const newRoutePath = {
+            streetName: "",
+            latLong: "",
+            routeId: 0,
+            order: 0
+        }
+        getRoutePath(routeObj.origin, routeObj.destination)
+            .then(arrayOfStreetNames => {
+                return arrayOfStreetNames.map(streetName => {
+                    // Runs each street name string through the geocoder API to get the lat/long
+                    return getLatLong(streetName)
+                        .then(resObj => {
+                            newRoutePath.streetName = streetName
+                            resObj.items.forEach(streetNameObj => {
+                                // Splices the origin and destination string into an array of strings
+                                const splitOrigin = origin.split(" ")
+                                const splitDestination = destination.split(" ")
+
+                                // returns an array of a single string representing city name
+                                const originCity = splitOrigin.splice(-3, 1)
+                                const destinationCity = splitDestination.splice(-3, 1)
+                                if (streetNameObj.title.toLowerCase().includes(`${originCity[0].toLowerCase()}`) ||
+                                    streetNameObj.title.toLowerCase().includes(`${destinationCity[0].toLowerCase()}`)) {
+                                    newRoutePath.latLong = (Object.values(streetNameObj.position))
+                                }
+                                newRoutePath.routeId = routeObj.id
+                                newRoutePath.order++
+                                debugger
+                                postRoutePath(newRoutePath)
+                            })
+                        })
+                })
+            })
+    }
+
+    const postRoutePath = (routePathObj) => {
+        return fetch(`http://localhost:8088/paths`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(routePathObj)
+        })
     }
 
     return (
