89c51594954bc4ea0cda71b2849bef19eb9ff078
kjk1325e <kaitlinjkelley@gmail.comm>
2021-04-09 15:47:50 -0500

    tried endpoints separated as individual files

--- src/components/auth/authSettings.js
+++ src/components/auth/authSettings.js
@@ -1,7 +1,7 @@
 // If your json-server API URL or endpoint is different, please change it below!
 export const authApi = {
-  localApiBaseUrl: "https://kaitlinjkelley.github.io/data/open_data.geojson",
-  endpoint: "users"
+  localApiBaseUrl: "https://kaitlinjkelley.github.io",
+  endpoint: "users.geojson"
 }
 
 // The user id is saved under the key app_user_id in session Storage. Change below if needed!
